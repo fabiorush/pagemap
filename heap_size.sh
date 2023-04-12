646a36058525176afe3d3916caf3be9f7c98fbac
Fabio Urquiza <xfaurqui@amazon.com>
2023-04-12 13:49:24 -0300

    Multi pid

--- heap_size.sh
+++ heap_size.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-PAGES=$(./pagemap2 $1 | grep -e 'file/shared 0 .*present 1.*heap' | wc -l)
-if [ $PAGES -ge 262144 ]
-then
-  echo Heap size: $(bc <<< "scale=3; $PAGES * 4 / 1024 / 1024") G
-else
-  echo Heap size: $(bc <<< "scale=3; $PAGES * 4 / 1024") M
-fi
+PIDS=ps -elf | awk '{ if ($0 ~ /openresty\/nginx/) cont = 1; if (cont && $16 == "worker") print $4 }'
+
+for PID in PIDS
+do
+  PAGES=$(./pagemap2 $PID | grep -e 'file/shared 0 .*present 1.*heap' | wc -l)
+  if [ $PAGES -ge 262144 ]
+  then
+    echo $PID Heap size: $(bc <<< "scale=3; $PAGES * 4 / 1024 / 1024") G
+  else
+    echo $PID Heap size: $(bc <<< "scale=3; $PAGES * 4 / 1024") M
+  fi
+done
