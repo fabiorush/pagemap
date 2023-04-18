ec2ed4e5a763e954fe31f8fb598d5068303489c3
Fabio Urquiza <xfaurqui@amazon.com>
2023-04-18 10:26:11 -0300

    Add RSS and date

--- heap_size.sh
+++ heap_size.sh
@@ -1,17 +1,23 @@
 #! /bin/bash
 
-PIDS=$(ps -elf | awk '{ if ($0 ~ /openresty\/nginx/) cont = 1; if (cont && $16 == "worker") print $4 }')
+PIDS=$(ps -elf | awk '{ if ($0 ~ /openresty\/nginx/) cont = 1; else if (cont && $16 == "worker") print $4; else cont = 0 }')
 
+date
 for PID in $PIDS
 do
+  RSS=$(ps auxw | awk "\$2 == $PID { print \$6 }")
   PAGES=$(./pagemap2 $PID | grep -e 'file/shared 0 .*present 1.*heap' | wc -l)
   if [ $PAGES -ge 262144 ]
   then
-    SIZE=$(bc <<<"scale=3; $PAGES * 4 / 1024 / 1024")
-    SIZE="$SIZE GB"
+    HEAP_SIZE=$(bc <<<"scale=3; $PAGES * 4 / 1024 / 1024")
+    HEAP_SIZE="$HEAP_SIZE GB"
+    RSS_SIZE=$(bc <<<"scale=3; $RSS / 1024 / 1024")
+    RSS_SIZE="$RSS_SIZE GB"
   else
-    SIZE=$(bc <<<"scale=3; $PAGES * 4 / 1024")
-    SIZE="$SIZE MB"
+    HEAP_SIZE=$(bc <<<"scale=3; $PAGES * 4 / 1024")
+    HEAP_SIZE="$HEAP_SIZE MB"
+    RSS_SIZE=$(bc <<<"scale=3; $RSS / 1024")
+    RSS_SIZE="$RSS_SIZE MB"
   fi
-  echo $PID Heap size: $SIZE
+  echo Worker $PID - RSS size: $RSS_SIZE - Heap size: $HEAP_SIZE
 done
